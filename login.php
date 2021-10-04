24eba3b8128ce133e69af303b9921feebef64e20
cyberawarenessmonth2021 <91869578+cyberawarenessmonth2021@users.noreply.github.com>
2021-10-04 12:28:42 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+
+echo '<html><head><title>Hamara Bank</title><link rel="stylesheet" href="style1.css"><meta http-equiv="Cache-control" content="public"></head><body> <div class="centered"><div id="in" class="border1">';
+ $otp=$_REQUEST['otp'];
+  if($otp=="8200")
+ {
+	echo "<p class='s3'> Welcome Alice!<br> Flag is 'BlockTheUserAfterCertainUnsuccessfulAttempts'</p>";
+	echo "<INPUT Type='submit' VALUE='Logout' onClick='history.go(-1);return true;'></br>"; 
+	}	 
+ }
+ 
+else{
+	echo "<p class='s3'>Incorrect OTP.</p>";
+	echo "<INPUT Type='submit' VALUE='Back' onClick='history.go(-1);return true;'></br>"; 
+}
+ 
+?>
