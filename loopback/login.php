1cc86d682a511185ed49efa558ac4d2b04250485
cyberawarenessmonth2021 <91869578+cyberawarenessmonth2021@users.noreply.github.com>
2021-10-04 13:22:41 +0530

    Add files via upload

---
+++ loopback/login.php
@@ -0,0 +1,15 @@
+<?php
+echo '<html><head><title>Cyber Awareness Month</title><link rel="stylesheet" href="style1.css"><meta http-equiv="Cache-control" content="public"></head><body> <div class="centered"><div id="in" class="border1">';
+ $otp=$_REQUEST['ip'];
+  if($otp=="169.254.169.254")
+ {
+	echo "<p class='s3'> Welcome Alice!<br> Flag is 'EnableIMDSv2inAWSEC2Instances'</p>";
+	echo "<INPUT Type='submit' VALUE='Close' onClick='window.close()'></br>";  
+ }
+ 
+else{
+	echo "<p class='s3'>Access denined</p>";
+	echo "<INPUT Type='submit' VALUE='Back' onClick='history.go(-1);return true;'></br>"; 
+}
+ 
+?>
