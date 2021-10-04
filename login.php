7dd04eeaf49c393dde2752b70b049d412ce6168e
cyberawarenessmonth2021 <91869578+cyberawarenessmonth2021@users.noreply.github.com>
2021-10-04 13:00:47 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,13 +1,10 @@
 <?php
-error_reporting(0);
-
 echo '<html><head><title>Cyber Bank</title><link rel="stylesheet" href="style1.css"><meta http-equiv="Cache-control" content="public"></head><body> <div class="centered"><div id="in" class="border1">';
  $otp=$_REQUEST['otp'];
   if($otp=="8200")
  {
 	echo "<p class='s3'> Welcome Alice!<br> Flag is 'BlockTheUserAfterCertainUnsuccessfulAttempts'</p>";
-	echo "<INPUT Type='submit' VALUE='Logout' onClick='history.go(-1);return true;'></br>"; 
-	}	 
+	echo "<INPUT Type='submit' VALUE='Logout' onClick='history.go(-1);return true;'></br>";  
  }
  
 else{
